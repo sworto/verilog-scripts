9bd76fcd040c12d2b14d334699bc3583615e3c27
Shivam <shivamdubey252005@outlook.com>
2025-09-29 21:24:51 +0530

    Added the single module compiling script.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "Enter filename: "
-read FILENAME
-iverilog FILENAME".v"
+echo -n "Enter Modulename: "
+read MODULE
+iverilog $MODULE.v
+yosys -p "read_verilog $MODULE.v; hierarchy -check; proc; opt; write_json $MODULE.json"
+netlistsvg $MODULE.json -o $MODULE"_RTL.html" --background white --stroke-width 5
+rm -r $MODULE.json a.out
+DESIGN=$MODULE"_RTL.html"
+chmod +x $DESIGN
+xdg-open $DESIGN
+clear
